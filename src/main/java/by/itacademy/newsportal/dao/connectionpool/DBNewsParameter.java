5e5509544dcf4ac4f85d1dd366935e03ef427aea
UmidAhmad <69048452+umid94@users.noreply.github.com>
2021-09-20 09:42:48 +0500

    Delete src/main directory

--- src/main/java/by/itacademy/newsportal/dao/connectionpool/DBNewsParameter.java
+++
@@ -1,9 +0,0 @@
-package by.itacademy.newsportal.dao.connectionpool;
-
-public class DBNewsParameter {
-	public static final String DB_NEWS_DRIVER = "db.driver";
-	public static final String DB_NEWS_URL = "db.url";
-	public static final String DB_NEWS_USER = "db.user";
-	public static final String DB_NEWS_PASSWORD = "db.password";
-	public static final String DB_NEWS_POOLSIZE = "db.poolsize";
-}
